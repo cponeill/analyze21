2534622746e9c181ef8311dc9ea73e1b9afcd3c0
cponeill <caseyoneill78@hotmail.com>
2016-09-11 11:41:39 -0400

    changed from linux command tool to API call.

--- analyze21.py
+++ analyze21.py
@@ -11,17 +11,18 @@ requests = BitTransferRequests(wallet)
 # server address
 server_url = 'http://10.244.107.98:4002/'
 
-@click.command()
-@click.argument('text')
-def cli(text):
+#@click.command()
+#@click.argument('text')
+#def cli(text):
+def buy_machine():
 
-    #input_text = input("Enter some text:\n")
+    input_text = input("Enter some text:\n")
     sel_url = server_url+'machine?text={0}'
-    #response = requests.get(url=sel_url.format(input_text))
-    response = requests.get(url=sel_url.format(text))
-    click.echo(response.text)
+    response = requests.get(url=sel_url.format(input_text))
+    #response = requests.get(url=sel_url.format(text))
+    #click.echo(response.text)
 
-    #print(response.text)
+    print(response.text)
 
-#if __name__=='__main__':
-    #buy_machine()
+if __name__=='__main__':
+    buy_machine()
